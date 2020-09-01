414cdda6325b1dc1d19b85e36782b74f100e5ec4
Prateek Kaushik <kaushikprateek11@gmail.com>
2020-09-01 20:00:39 +0530

    router gaurd

--- blog project/src/router.js
+++ blog project/src/router.js
@@ -7,9 +7,15 @@ import Album from './components/gallery/album'
 import Photo from './components/gallery/photo'
 
 export default [
-  { path: '/', component: Home },
+  { path: '/', component: Home, name: 'home' },
   { path: '/blog', component: Blog },
   { path: '/blog/:id', component: Detail },
   { path: '/gallery', component: Album },
-  { path: '/gallery/:id', component: Photo },
+  { path: '/gallery/:id', component: Photo,
+    // beforeEnter: (to, from, next) => {
+    //   console.log(to);
+    //   console.log(from);
+    //   next({ name: 'home' })
+    // }
+  },
 ]
